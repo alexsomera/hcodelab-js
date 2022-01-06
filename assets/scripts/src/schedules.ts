57470846e7af9181dd0294e73b49ade06d313d78
Alex Somera <alexsomera@gmail.com>
2022-01-05 21:16:23 -0300

    scripts

--- assets/scripts/src/schedules.ts
+++ assets/scripts/src/schedules.ts
@@ -3,5 +3,9 @@ const pageSchedules = document.querySelector("#schedules");
 if (pageSchedules) {
 
     const btnCancel = pageSchedules.querySelector(".cancel") as HTMLButtonElement;
+
+    const list =  pageSchedules.querySelector("ul") as HTMLUListElement;
+
+    const listItem =  list.querySelector("li") as HTMLLIElement;
     
 }
